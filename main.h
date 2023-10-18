c5c22178f86fe83442be32aa4eedbdb316ee5907
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-10-17 19:51:59 -0700

    buffer

--- main.h
+++ main.h
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 int _printf(const char *format, ...);
 int print_integer(int num, int *char_print);
 int print_string(char *str, int *char_print);
